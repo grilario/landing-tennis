530320ca9aa86040081fe5c15c2d201792453fe8
Luis Fernando <luisfernandinho094@gmail.com>
2023-02-01 21:36:30 -0300

    feat: faq section

---
+++ src/components/Details/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { CaretDown, CaretUp } from "phosphor-react";
+
+import styles from "./styles.module.css";
+
+interface IDetailsProps {
+  title: string;
+  text: string;
+}
+
+export default function Details({ title, text }: IDetailsProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <details
+      className={styles.details}
+      onToggle={(e) => setIsOpen(e.currentTarget.open)}
+    >
+      <summary>
+        <span>{title}</span>
+        {isOpen ? (
+          <CaretUp size={28} color="#fff" />
+        ) : (
+          <CaretDown size={28} color="#fff" />
+        )}
+      </summary>
+      <p>{text}</p>
+    </details>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import tennis from "@src/../public/tennis.png";
 import tennisVariantRed from "@src/../public/variant-red.png";
 import tennisVariantRed2 from "@src/../public/variant-red-2.png";
 import tennisVariantGold from "@src/../public/variant-gold.png";
+import Details from "@src/components/Details";
 
 export default function Home() {
   return (
@@ -49,6 +50,16 @@ export default function Home() {
           <div>
             <span>03</span>
             <p>Tecnologia avançada para melhor performance</p>
+          </div>
+        </section>
+        <section className={styles.faq}>
+          <h2>FAQ</h2>
+          <div>
+            <Details title="Qual é o material utilizado na fabricação do Jordan?" text="O Jordan é feito de materiais de alta qualidade, incluindo couro sintético e tecidos respiráveis."/>
+            <Details title="Qual é a garantia oferecida para o produto?" text="Oferecemos uma garantia de 12 meses contra defeitos de fabricação."/>
+            <Details title="O Jordan é confortável para usar durante todo o dia?" text=" Sim, o Jordan foi projetado com tecnologia avançada para garantir conforto durante todo o dia."/>
+            <Details title="Existe algum limite de peso para usar esse tênis?" text="Não há um limite de peso específico, mas o Jordan foi projetado para suportar até 220 libras."/>
+            <Details title="O Jordan é resistente à água?" text="Não, o Jordan não é resistente à água e não é recomendado usá-lo em condições molhadas."/>
           </div>
         </section>
       </main>
